2b0f0c59441d7ea69866251af7700f561b381b52
Andrés <apena@cualit.com>
2024-12-07 18:49:25 -0300

    Schema instance test

--- jest/schema/initialization.test.ts
+++ jest/schema/initialization.test.ts
@@ -1,6 +1,6 @@
 import { Schema } from "../../lib/elastickbird";
 
 test("Schema instance is an object", () => {
-  const ExampleModel = new Schema();
+  const ExampleModel = new Schema({ alias: "example" });
   expect(ExampleModel).toBeInstanceOf(Object);
 });
