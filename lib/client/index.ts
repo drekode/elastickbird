beac6d306d0c946cec0a65f8f9bc36c7740a1444
Andrés <apena@cualit.com>
2024-12-07 18:49:00 -0300

    Client getter

--- lib/client/index.ts
+++ lib/client/index.ts
@@ -2,6 +2,13 @@ import type { ClientOptions } from "@elastic/elasticsearch";
 import type { ElastickBirdClient } from "./client.d";
 import Client from "./client";
 
+let client: ElastickBirdClient | null = null;
 export const connect = function (options: ClientOptions): ElastickBirdClient {
-  return new Client(options);
+  return client = new Client(options);
 };
+export const getClient = function (): ElastickBirdClient {
+  if (!client) {
+    throw new Error('Client not connected');
+  }
+  return client;
+}
